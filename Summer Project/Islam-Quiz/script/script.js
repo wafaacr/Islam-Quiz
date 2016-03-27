c1cbf4bb96c2337be021040e4be1256c67e88ef3
wafaacr <cherow01@wairaka.com>
2016-03-28 12:49:57 +1300

    ADDED ------ - New topic under Foundation of Faith content.

--- Summer Project/Islam-Quiz/script/script.js
+++ Summer Project/Islam-Quiz/script/script.js
@@ -27,4 +27,47 @@ $(document).ready(function(){
 	$("#topic4").click(function(){
 		$("#content4").toggle();
 	});
+	
+});
+
+$(function() {
+
+    var newHash      = "",
+        $mainContent = $("#main-content"),
+        $pageWrap    = $("#page-wrap"),
+        baseHeight   = 0,
+        $el;
+        
+    $pageWrap.height($pageWrap.height());
+    baseHeight = $pageWrap.height() - $mainContent.height();
+    
+    $("nav").delegate("a", "click", function() {
+        window.location.hash = $(this).attr("href");
+        return false;
+    });
+    
+    $(window).bind('hashchange', function(){
+    
+        newHash = window.location.hash.substring(1);
+        
+        if (newHash) {
+            $mainContent
+                .find("#guts")
+                .fadeOut(500, function() {
+                    $mainContent.hide().load(newHash + " #guts", function() {
+                        $mainContent.fadeIn(200, function() {
+                            $pageWrap.animate({
+                                height: baseHeight + $mainContent.height() + "px"
+                            });
+                        });
+                        $("nav a").removeClass("current");
+                        $("nav a[href='"+newHash+"']").addClass("current");
+                    });
+                });
+        };
+        
+    });
+    
+    $(window).trigger('hashchange');
+
 });
